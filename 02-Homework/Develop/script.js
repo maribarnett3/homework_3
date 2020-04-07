4b1b43dd04049368ba885e831354934ee2c11761
Mariana Barnett <marianabarnett3@gmail.com>
2020-04-07 00:33:26 -0400

    homework 3

--- 02-Homework/Develop/script.js
+++ 02-Homework/Develop/script.js
@@ -27,18 +27,34 @@ function getPasswordOptions() {
   options.passwordLength = passwordLength
   options.characterTypes = prompt("Character types desired comma separated: lowercase,uppercase,numeric,special").split(",");
   supportedTypes = "lowercase,uppercase,numeric,special".split(",");
-  options.characterTypes.forEach(type => {
-    if (supportedTypes.indexOf(type) == -1) {
-      alert(type + " is not a valid password character choice")
-      options = null;
-      return;
-    }
-  });
+  if (options.characterTypes[0])
+    options.characterTypes.forEach(type => {
+      if (supportedTypes.indexOf(type) == -1) {
+        alert(type + " is not a valid password character choice")
+        options = null;
+        return;
+      }
+    });
 
 }
 
-function generatePassword(passwordLength) {
-  return "password"
+function generatePassword() {
+  var length = options.passwordLength,
+    charset = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
+  if (options.characterTypes.indexOf("numeric") != -1) {
+    charset = "0123456789"
+  }
+  if (options.characterTypes.indexOf("special") != -1) {
+    charset += "$%^*@~"
+  }
+
+  retVal = "";
+  for (var i = 0, n = charset.length; i < length; ++i) {
+    retVal += charset.charAt(Math.floor(Math.random() * n));
+  }
+
+
+  return retVal;
 }
 
 // Add event listener to generate button
