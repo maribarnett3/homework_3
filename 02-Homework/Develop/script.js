289d2f0de32020c1e158f33b9816926b0822d86b
Mariana Barnett <marianabarnett3@gmail.com>
2020-04-07 00:21:15 -0400

    “😫😫😫😫

--- 02-Homework/Develop/script.js
+++ 02-Homework/Develop/script.js
@@ -3,12 +3,45 @@ var generateBtn = document.querySelector("#generate");
 
 // Write password to the #password input
 function writePassword() {
-  var password = generatePassword();
-  var passwordText = document.querySelector("#password");
+  options = {}
+  getPasswordOptions()
+  if (options) {
+    var password = generatePassword();
+    var passwordText = document.querySelector("#password");
 
-  passwordText.value = password;
+    passwordText.value = password;
+  }
 
+}
+
+var options = {}
+var characterTypes;
+// function getPasswordOptions() {
+function getPasswordOptions() {
+  var passwordLength = parseInt(prompt("how many character would you like your password to be?"));
+  if (passwordLength < 8 || passwordLength > 128) {
+    alert("Password must be between 8 and 128 characters")
+    options = null;
+    return
+  }
+  options.passwordLength = passwordLength
+  options.characterTypes = prompt("Character types desired comma separated: lowercase,uppercase,numeric,special").split(",");
+  supportedTypes = "lowercase,uppercase,numeric,special".split(",");
+  options.characterTypes.forEach(type => {
+    if (supportedTypes.indexOf(type) == -1) {
+      alert(type + " is not a valid password character choice")
+      options = null;
+      return;
+    }
+  });
+
+}
+
+function generatePassword(passwordLength) {
+  return "password"
 }
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
+// getPasswordOptions ();
+//
